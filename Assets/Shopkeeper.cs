e30d1fcfda5b06d5e9e06255b1763b48b3e71acf
athernalz <97809771+athernalz@users.noreply.github.com>
2024-03-18 17:54:46 +0200

    Shop text

--- Assets/Shopkeeper.cs
+++ Assets/Shopkeeper.cs
@@ -11,27 +11,43 @@ public class Shopkeeper : MonoBehaviour
     public TMP_Text shopText;
     public Button[] shopButtons;
     public Image darkenScreen;
+    public Animator animator;
 
     private void Start()
     {
         shopText.gameObject.SetActive(false);
     }
 
+    private bool isPlayerInShopRange = false;
+
     private void Update()
     {
-        // If player is in the shopkeeper area
-        if (Vector3.Distance(player.transform.position, shopTriggerArea.transform.position) <= shopTriggerArea.transform.localScale.x / 2f)
-        {
-            shopText.gameObject.SetActive(true);
+        bool playerCurrentlyInRange = Vector3.Distance(player.transform.position, shopTriggerArea.transform.position) <= shopTriggerArea.transform.localScale.x / 2f;
 
-            // open the shop if player clicks b
-            if (Input.GetKeyDown(KeyCode.B))
+        // If the player has just entered the shop range
+        if (playerCurrentlyInRange && !isPlayerInShopRange)
+        {
+            Debug.Log("Enter");
+            isPlayerInShopRange = true;
+            animator.SetTrigger("enterShopRange");
+            shopText.gameObject.SetActive(true);
+        }
+        // If the player has just exited the shop range
+        else if (!playerCurrentlyInRange && isPlayerInShopRange)
+        { 
+            Debug.Log("Exit");
+            isPlayerInShopRange = false;
+            animator.SetTrigger("exitShopRange");
+        }
+
+        // Open the shop if player clicks 'B' and is in range
+        if (isPlayerInShopRange && Input.GetKeyDown(KeyCode.B))
+        {
+            Debug.Log("Open Shop");
+            darkenScreen.gameObject.SetActive(true);
+            foreach (Button button in shopButtons)
             {
-                darkenScreen.gameObject.SetActive(true);
-                foreach (Button button in shopButtons)
-                {
-                    button.gameObject.SetActive(true);
-                }
+                button.gameObject.SetActive(true);
             }
         }
     }
